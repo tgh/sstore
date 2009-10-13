91b2a617cd386e7b943aeb2c6cd91760b1a22aca
Tyler Hayes <tgh@pdx.edu>
2009-10-13 10:05:05 -0700

    added header comment and changed function names to sstore_*

--- sstore.c
+++ sstore.c
@@ -1,23 +1,24 @@
 /*                                                     
- * $Id: hello.c,v 1.5 2004/10/26 03:32:21 corbet Exp $ 
+ * (C) COPYRIGHT 2009 Tyler Hayes - tgh@pdx.edu
+ *
+ * sstore.c - the most useful device driver ever! 
  */                                                    
 
-#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void)
+static int sstore_init(void)
 {
 	printk(KERN_ALERT "Hello, world\n");
 	return 0;
 }
 
-static void hello_exit(void)
+static void sstore_exit(void)
 {
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(sstore_init);
+module_exit(sstore_exit);
