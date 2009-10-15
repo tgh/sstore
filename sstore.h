8c0282d8d9c7d61e5b81b2e668868b2da441dbf6
Tyler Hayes <tgh@pdx.edu>
2009-10-15 11:42:59 -0700

    added header comment and #include cdev.h in sstore.h

--- sstore.h
+++ sstore.h
@@ -1,6 +1,17 @@
+/*                                                     
+ * (C) COPYRIGHT 2009 Tyler Hayes - tgh@pdx.edu
+ *
+ * sstore.h 
+ */
+
+#include <linux/cdev.h> /* for the cdev struct */
+
+//----------------------------------------------------------------------------
+
 /* device major number (0 means it will be dynamically allocated by kernel)
  * in the init function */
 int SSTORE_MAJOR = 0;
+
 //number of devices that can be associated with this driver
 const int SSTORE_DEVICE_COUNT = 2;
 
@@ -18,4 +29,5 @@ struct blob {
 //the device structure
 struct sstore {
     struct blob * blob_list;
+    struct cdev * cdev;
 };
