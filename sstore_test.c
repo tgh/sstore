dc421a7765a1495d22756af7c6669c409aa564ff
Tyler Hayes <tgh@pdx.edu>
2009-10-27 17:03:58 -0700

    added buffer_container to test

--- sstore_test.c
+++ sstore_test.c
@@ -8,7 +8,14 @@ struct readWriteBuffer {
 
 int main ()
 {
+    char buffer_container[1024];
     struct readWriteBuffer buf;
+
+    buf.index = 5;
+    buf.size = 10;
+    buf->data = "Hello world";
+
+    
 
     return 0;
 }
