ce61a2d1d7d97f5d48565b825dd08b7a25c5a1de
root <root@tgh.(none)>
2009-10-29 16:43:44 -0700

    added open and close calls to test

--- sstore_test.c
+++ sstore_test.c
@@ -1,21 +1,26 @@
-#include <stdio.h>
-
 struct readWriteBuffer {
     int index;
     int size;
     char * data;
-}
+};
 
 int main ()
 {
+    int sstore_device;
     char buffer_container[1024];
     struct readWriteBuffer buf;
 
+    //test open
+    sstore_device = open("/dev/sstore0", "r");
+    //test read
+
+    //test write
     buf.index = 5;
     buf.size = 10;
-    buf->data = "Hello world";
+    buf.data = "Hello world";
 
-    
+    //test close
+    close(sstore_device);
 
     return 0;
 }
