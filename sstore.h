54bc7f8b049499e310e59bffd71f07d18972418c
Tyler <tgh@KidA.(none)>
2009-11-02 10:56:47 -0800

    added fd_count to sstore struct to sstore.h

--- sstore.h
+++ sstore.h
@@ -33,6 +33,13 @@ struct blob {
 
 //the device structure
 struct sstore {
+    /*
+     * fd_count keeps track of how many open file descriptors in user space are
+     * associated with the device represented by an instance of this struct.
+     * This is done so that the release function in the driver can shut down
+     * the device on the last close. (See "Linux Device Drivers" 3rd Ed. pg. 59)
+     */
+    unsigned int fd_count;
     unsigned int blob_count;
     struct blob * list_head;
     struct blob * current_blob;
