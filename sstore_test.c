c79ffba41c7beb69984f588cc08e092f6c926fa1
Tyler Hayes <tgh@pdx.edu>
2009-11-03 11:25:06 -0800

    added write test (commented out) to sstore_test.c

--- sstore_test.c
+++ sstore_test.c
@@ -10,9 +10,8 @@ struct readWriteBuffer {
 
 int main ()
 {
-    int sstore_device;
-    char buffer_container[1024];
-    struct readWriteBuffer buf;
+    int sstore_device;          //file descriptor for the device
+    struct readWriteBuffer buf; //buffer to be passed in through read and write
     int bytes_read = 0;
     int bytes_written = 0;
 
@@ -21,15 +20,32 @@ int main ()
     if (sstore_device < 0)
         perror("open");
 
-    //test read
+    //test read with arbitrary values
     buf.index = 2;
     buf.size = 10;
     buf.data = (char *) malloc(11);
-    bytes_read = read(sstore_device, &buf, 10);
+    if (!buf.data) {
+        printf("\nError in malloc: sstore_test.c\n");
+        return 0;
+    }   
+    bytes_read = read(sstore_device, &buf, buf.size);
     perror("read");
     printf("\namount read = %d, data read = %s\n", bytes_read, buf.data);
 
-    //test write
+/*
+    //test write with arbitrary values
+    buf.index = 7;
+    buf.size = 21;
+    free(buf.data);
+    buf.data = (char *) malloc(22);
+    if (!buf.data) {
+        printf("\nError in malloc: sstore_test.c\n");
+        return 0;
+    }
+    bytes_written = write(sstore_device, &buf, buf.size);
+    perror("write");
+    printf("\namount written = %d\n", bytes_written);
+*/
 
     //test close
     if (sstore_device == 0)
